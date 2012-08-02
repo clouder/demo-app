4e5c95912b4ea6315263e793f6cb245084fd2c84
Gumaro Melendez <cloudernew@gmail.com>
2012-08-02 14:58:11 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '65d80d7b79ac555aa204b8193f8f43c06980b4274b51e06d6597f52ace549f6560971ae0a006f712dddf05274be2c05c6c79650e31bbe7810c23b22737f76dd6'
